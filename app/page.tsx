739fb9b4fec87c24cfc3d39812b135e17c78820b
Pauline <pauline@MacBook-Air-3.local>
2026-02-26 19:41:32 +0100

    Initial PWA wrapper

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <iframe
+      src="https://www.villakvarndammen.se"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
